1edce83aee9499f58afbc8b5e72fbc65a2b1968d
Lester Tan <127372303+SunsetStories@users.noreply.github.com>
2023-05-07 16:00:35 +0800

    Mockup home page with mock data
    Displays username or name (TBD) and recent transactions

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,26 @@
+from flask import render_template
 from app import app
 #Contains the different URLs of the bank webapp
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'John'}
+    account_balance = 1
+    transactions = [
+        {
+            'id': "001",
+            'Date': "01/01/2023",
+            'To': "Jane",
+            'From': "John",
+            'Amount': 2
+        },
+        {
+            'id': "002",
+            'Date': "02/01/2023",
+            'To': "John",
+            'From': "Jane",
+            'Amount': 1
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, transactions=transactions)
